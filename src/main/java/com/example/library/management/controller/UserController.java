07952600ed9d30f3264c7e727a935bb842254cb9
HARSHITHASYAMALAPODURI <110627042+HARSHITHASYAMALAPODURI@users.noreply.github.com>
2025-06-04 00:23:04 +0530

    Create UserController.java

---
+++ src/main/java/com/example/library/management/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.library.controller;
+
+import com.library.model.User;
+import com.library.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElseThrow();
+        user.setName(userDetails.getName());
+        user.setMembershipType(userDetails.getMembershipType());
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
